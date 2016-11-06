3e8c21299d68c12097245f87202974fb62f2c646
turanegaku <turanegaku@gmail.com>
2016-11-06 15:24:20 +0900

    graph view by d3
    not only node and edge show the labels
    
    dynamic change able

--- docs/javascripts/search.js
+++ docs/javascripts/search.js
@@ -1,152 +1,268 @@
-/*globals $ d3 Materialize*/
+/* globals $ d3 */
+
+const INF = 1000;
+
+const svg = d3.select('body svg');
+const width = 960;
+const height = 600;
+const simulation = d3.forceSimulation()
+.velocityDecay(0.3)
+.force('link', d3.forceLink().id(d => d.id).distance(200).strength(1))
+.force('charge', d3.forceManyBody())
+.force('center', d3.forceCenter(width / 2, height / 2))
+.force('x', d3.forceX(d => width * d.rate).strength(0.02))
+;
 
 $(() => {
-    const search = $('#search');
-    const searching = $('.preloader-wrapper.small');
-    const result = d3.select('ul#result');
+    d3.select('svg').selectAll('g').exit().remove();
+
+    const search = $('button#search');
     const isrc = $('input#src');
     const idst = $('input#dst');
+    const depth = $('input#depth');
 
-    // カテゴリに含まれるアイテム
-    const category = new Map();
-    // 構成要素
-    const alchemy = new Map();
-    // 補完候補 Mapだとautocompleteで認識してくれない
-    const completes = {};
-    const completeg = {};
+    search.removeAttr('disabled');
 
-    $('.range-field>input#depth').mouseup(() => {
-        $('span.thumb').remove();
-        $('.range-field>input#depth').blur();
-    });
 
+    let n = 0;
+    const id = new Map();
+    const name = new Array();
+    const d = new Array();
+    let e = new Array();
 
     d3.csv('./acquaintance.csv', data => {
-        data.forEach(v => {
-            // アイテムは候補に含める
-            completes[v.name] = null;
-            let alchable = false;
-            // 構成要素
-            alchemy.set(v.name, new Array());
+        const classList = new Map();
+
+        // クラス数の調査
+        data.forEach((v, j) => {
+            id.set(v.name, j);
+            name.push(v.name);
             for (let i = 0; i < 4; i++) {
-                // カテゴリもゴールの候補に含める
-                const c = v['c' + i];
-                if (c) {
-                    completeg[c] = null;
-                    if (!category.has(c)) {
-                        category.set(c, new Array());
-                    }
-                    category.get(c).push(v.name);
-                }
-
-                // 構成要素
-                const m = v['m' + i];
-                if (m) {
-                    alchable = true;
-                    alchemy.get(v.name).push(m);
-                }
-            }
-            // 錬成可能ならゴールにも含める
-            if (alchable) {
-                completeg[v.name] = null;
-            }
-        });
-
-        $('input.autocompletes').autocomplete({
-            'data': completes
-        });
-        $('input.autocompleteg').autocomplete({
-            'data': completeg
-        });
-
-        searchRoute(true);
-
-        function searchRoute(first) {
-            result.selectAll('ul>li').remove();
-
-            const src = isrc.val();
-            const dst = idst.val();
-            const depth = parseInt($('input#depth').val(), 10);
-            if (!(src in completes && dst in completeg)) {
-                if (!first) {
-                    if (!src) {
-                        Materialize.toast('src を指定してください', 2000);
-                    } else if (!(src in completes)) {
-                        Materialize.toast(src + 'が見つかりませんでした', 2000);
-                    }
-                    if (!dst) {
-                        Materialize.toast('dst を指定してください', 2000);
-                    } else if (!(dst in completes)) {
-                        Materialize.toast(dst + 'が見つかりませんでした', 2000);
-                    }
-                }
-                searching.removeClass('active');
-                search.removeClass('disabled');
-                return;
-            }
-
-            // dst側から探索する
-            const que = new Array();
-            let pdepth = 0;
-            que.push(new Array(dst));
-            while (que.length > 0) {
-                const p = que.shift();
-                const pp = p[p.length - 1];
-                // 見つかった
-                if (pp == src) {
-                    p.reverse();
-
-                    result.append('li')
-                        .classed('collection-item', true)
-                        .selectAll('span')
-                        .data(p).enter()
-                        .append('span')
-                        .text(d => {
-                            return d + ' ';
-                        });
+                const cs = v['c' + i];
+                if (!cs) {
                     continue;
                 }
-                // 長すぎる
-                if (p.length > depth * 2) {
-                    break;
+                if (!classList.has(cs)) {
+                    classList.set(cs, new Array());
                 }
-                if (pdepth < p.length) {
-                    result.append('li')
-                        .classed('collection-item', true)
-                        .text(Math.floor(p.length / 2 + 1));
-                    pdepth = p.length;
-                }
-
-                const m = alchemy.has(pp) ? alchemy.get(pp) : new Array(pp);
-                m.forEach(v => {
-                    if (category.has(v)) {
-                        category.get(v).forEach(vv => {
-                            if (!(vv in p)) {
-                                const sp = p.concat();
-                                sp.push(v, vv);
-                                que.push(sp);
-                            }
-                        });
-                    } else if (!(v in p)) {
-                        const sp = p.concat();
-                        sp.push(v, v);
-                        que.push(sp);
-                    }
-                });
+                classList.get(cs).push(j);
             }
-            searching.removeClass('active');
-            search.removeClass('disabled');
+        });
+
+        // グラフの初期化
+        n = id.size;
+        d.length = n;
+        e.length = n;
+        for (let i = 0; i < n; i++) {
+            d[i] = new Array(n);
+            d[i].fill(INF);
+            e[i] = new Array();
         }
 
-        search.click(() => {
-            setTimeout(() => {
-                search.addClass('disabled');
-                searching.addClass('active');
-
-                setTimeout(() => {
-                    searchRoute();
-                }, 10);
-            });
+        // 初期エッジ
+        data.forEach((v, j) => {
+            for (let i = 0; i < 4; i++) {
+                const ms = v['m' + i];
+                if (!ms) {
+                    continue;
+                }
+                if (classList.has(ms)) {
+                    classList.get(ms).forEach(m => {
+                        d[m][j] = 1;
+                        e[m].push({id: j, as: ms});
+                    });
+                } else {
+                    const m = id.get(ms);
+                    d[m][j] = 1;
+                    e[m].push({id: j, as: ms});
+                }
+            }
         });
+
+        // Floyd–Warshall
+        for (let i = 0; i < n; i++) {
+            for (let j = 0; j < n; j++) {
+                for (let k = 0; k < n; k++) {
+                    d[j][k] = Math.min(d[j][k], d[j][i] + d[i][k]);
+                }
+            }
+        }
+
+        resultShow();
+    });
+
+    isrc.val('ハチミツ');
+    idst.val('先見の水晶玉');
+
+    depth.on('change', () => resultShow(depth.val()));
+
+
+    function resultShow(l) {
+        svg.selectAll('g').remove();
+
+        const src = isrc.val();
+        const dst = idst.val();
+        const iis = id.get(src);
+        const iid = id.get(dst);
+        // console.log(src, dst);
+        // console.log(iis, iid);
+        // console.log(name[iis], name[iid]);
+        if (!iis || !iid) {
+            console.log('plz set src and dst');
+            console.log('src', iis);
+            console.log('dst', iid);
+            return;
+        }
+        const limit = l || d[iis][iid];
+        if (limit == 0) {
+            return;
+        }
+        console.log('limit', limit);
+        depth[0].MaterialSlider.change(limit);
+
+        const nodes = new Array();
+        const links = new Array();
+        nodes.push({id: iis, name: src, end: true, rate: 0, r: 10});
+        nodes.push({id: iid, name: dst, end: true, rate: 1, r: 10});
+        for (let i = 0; i < n; i++) {
+            if (d[iis][i] + d[i][iid] <= limit) {
+                nodes.push({
+                    id: i,
+                    name: name[i],
+                    end: false,
+                    rate: d[iis][i] / limit,
+                    r: 10 - 8 * Math.min(d[iis][i], d[i][iid]) / limit
+                });
+            }
+        }
+        nodes.forEach(u => {
+            if (typeof u.rate === 'undefined') {
+                u.rate = d[iis][u.id] / limit;
+            }
+            console.log(u);
+        });
+        nodes.forEach(u => {
+            const edge = new Map();
+            e[u.id].forEach(v => {
+                if (nodes.find(n => n.id === v.id)) {
+                    if (!edge.has(v.id)) {
+                        edge.set(v.id, new Array());
+                    }
+                    edge.get(v.id).push(v.as);
+                }
+            });
+            edge.forEach((l, v) => {
+                console.log({source: u.id, target: v, as: l.join(' '), rev: d[v][u.id] === 1});
+                links.push({source: u.id, target: v, as: l.join(' '), rev: d[v][u.id] === 1});
+            });
+            // links.push({source: u.id, target: v.id, as: v.as});
+        });
+
+
+        const nodelabel = svg.append('g')
+        .attr('class', 'nodelabels')
+        .selectAll('text')
+        .data(nodes)
+        .enter().append('text')
+        .text(d => d.name);
+
+        const node = svg.append('g')
+        .attr('class', 'nodes')
+        .selectAll('circle')
+        .data(nodes)
+        .enter().append('circle')
+        .attr('fill', d => d3.interpolateCool(d.rate))
+        .attr('r', d => d.r)
+        .call(
+            d3.drag()
+            .on('start', dragstarted)
+            .on('drag', dragged)
+            .on('end', dragended)
+        );
+
+        const link = svg.append('g')
+        .attr('class', 'links')
+        .attr('marker-end', 'url(#m_ar)')
+        .selectAll('path')
+        .data(links)
+        .enter().append('path')
+        .attr('id', (d, i) => 'linkpath' + i);
+
+        const linklabels = svg.append('g')
+        .attr('class', 'linklabels')
+        .selectAll('text')
+        .data(links)
+        .enter()
+        .append('text')
+        .attr('id', (d, i) => 'linklabel' + i)
+        .attr('dx', 80)
+        .attr('dy', 0);
+
+        linklabels.append('textPath')
+        .attr('xlink:href', (d, i) => '#linkpath' + i)
+        .text(d => console.log(d.as) || d.as);
+
+        simulation.stop();
+        simulation.alpha(1);
+        simulation
+        .nodes(nodes)
+        .on('tick', ticked);
+
+        simulation.force('link')
+        .links(links);
+
+        simulation.restart();
+
+
+        function linkArc(d) {
+            const dx = d.target.x - d.source.x;
+            const dy = d.target.y - d.source.y;
+            if (!d.rev) {
+                return 'M' + d.source.x + ',' + d.source.y + 'L' + d.target.x + ',' + d.target.y;
+            }
+            const dr = Math.sqrt(dx * dx + dy * dy);
+            return 'M' + d.source.x + ',' + d.source.y + 'A' + dr + ',' + dr + ' 0 0,1 ' + d.target.x + ',' + d.target.y;
+        }
+
+
+        function ticked() {
+            link.attr('d', linkArc);
+
+            node
+            .attr('cx', d => d.x)
+            .attr('cy', d => d.y);
+
+            nodelabel
+            .attr('x', d => d.x)
+            .attr('y', d => d.y);
+        }
+    }
+
+
+    search.click(() => {
+        search.attr('disabled', true);
+        resultShow();
+        search.removeAttr('disabled');
     });
 });
+
+function dragstarted(d) {
+    if (!d3.event.active) {
+        simulation.alphaTarget(0.3).restart();
+    }
+    d.fx = d.x;
+    d.fy = d.y;
+}
+
+function dragged(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
+}
+
+function dragended(d) {
+    if (!d3.event.active) {
+        simulation.alphaTarget(0);
+    }
+    d.fx = null;
+    d.fy = null;
+}
